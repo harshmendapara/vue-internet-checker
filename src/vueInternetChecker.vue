a6cb4469410334593652fce3ff5044646357a8eb
eh. sh <sh.ehsan74.es@gmail.com>
2021-12-23 19:10:20 +0330

    check network connection in created

--- src/vueInternetChecker.vue
+++ src/vueInternetChecker.vue
@@ -1,18 +1,24 @@
 <template>
   <div />
 </template>
-<script>
-  export default {
-    created() {
-      window.addEventListener('online', (event) => {
-        this.$emit('status', true);
-        this.$emit('event', event);
-      });
 
-      window.addEventListener('offline', (event) => {
-        this.$emit('status', false);
-        this.$emit('event', event);
-      });
-    },
-  };
+<script>
+export default {
+  created() {
+    if (!window.navigator.onLine) {
+      this.$emit('status', false)
+    } else {
+      this.$emit('status', true)
+    }
+    window.addEventListener('online', (event) => {
+      this.$emit('status', true)
+      this.$emit('event', event)
+    })
+
+    window.addEventListener('offline', (event) => {
+      this.$emit('status', false)
+      this.$emit('event', event)
+    })
+  },
+}
 </script>
